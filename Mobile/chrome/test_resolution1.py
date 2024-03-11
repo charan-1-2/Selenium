ac883611ca1c523657baf9582f25767eaa1c9bf6
charan-1-2 <naikcharan484@gmail.com>
2024-03-11 09:07:19 +0530

    added to git

---
+++ Mobile/chrome/test_resolution1.py
@@ -0,0 +1,40 @@
+import time
+
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+def test_res1():
+    chrom_option = Options()
+    chrom_option.add_argument("--disable-notifications")
+    driver = Chrome(options=chrom_option)
+    driver.implicitly_wait(10)
+    def go_back(driver):
+        time.sleep(1)
+        driver.back()
+        time.sleep(1)
+    driver.get("https://www.getcalley.com/page-sitemap.xml")
+    driver.set_window_size(360,640)
+
+    driver.find_element(By.XPATH,"//a[text()='https://www.getcalley.com/']").click()
+    driver.get_screenshot_as_file("./homepage1.png")
+    go_back(driver)
+
+    driver.find_element(By.XPATH, "//a[text()='https://www.getcalley.com/calley-call-from-browser/']").click()
+    driver.get_screenshot_as_file("./homepage2.png")
+    go_back(driver)
+
+    driver.find_element(By.XPATH, "//a[text()='https://www.getcalley.com/calley-pro-features/']").click()
+    driver.get_screenshot_as_file("./homepage3.png")
+    go_back(driver)
+
+    driver.find_element(By.XPATH, "//a[text()='https://www.getcalley.com/best-auto-dialer-app/']").click()
+    driver.get_screenshot_as_file("./homepage4.png")
+    go_back(driver)
+
+    driver.find_element(By.XPATH, "//a[text()='https://www.getcalley.com/how-calley-auto-dialer-app-works/']").click()
+    driver.get_screenshot_as_file("./homepage5.png")
+    time.sleep(2)
+
+    driver.quit()
+
